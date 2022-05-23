5157e942a4787aff31e19f969ef0fde2808fd486
chusi <4us.in.ua@gmail.com>
2022-05-23 15:34:34 +0300

    Сделал все что мог потратил 2 дня

---
+++ Hw6cs/Class1.cs
@@ -0,0 +1,126 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hw6cs
+{
+    internal class Class1
+    {
+        class Student
+        {
+            public string lastName;
+            public string firstName;
+            public string university;
+            public string faculty;
+            public int course;
+            public string department;
+            public int group;
+            public string city;
+            public int age;
+            // Создаем конструктор
+            public Student(string firstName, string lastName, string university,
+            string faculty, string department,int age, int course, int group, string city)
+            {
+                this.lastName = lastName;
+                this.firstName = firstName;
+                this.university = university;
+                this.faculty = faculty;
+                this.department = department;
+                this.course = course;
+                this.age = age;
+                this.group = group;
+                this.city = city;
+            }
+        }
+        static int MyDelegat(Student st1, Student st2)
+        {
+            return String.Compare(st1.firstName, st2.firstName);
+
+        }
+      
+
+      
+        static void Main(string[] args)
+        {
+            int studentOnFifthCourse = 0;
+            int studentOnSixthCourse = 0;
+            int bakalavr = 0;
+            int magistr = 0;
+            List<Student> list = new List<Student>();
+            // Создаем список студентов
+            DateTime dt = DateTime.Now;
+            StreamReader sr = new StreamReader("students_6.csv");
+            while (!sr.EndOfStream)
+            {
+                try
+                {
+                    string[] s = sr.ReadLine().Split(';');
+                    // Добавляем в список новый экземпляр класса Student
+                    list.Add(new
+                    Student(s[0], s[1], s[2], s[3], s[4], int.Parse(s[5]), int.Parse(s[6]), int.Parse(s[7]), s[8]));
+                    // Одновременно подсчитываем количество бакалавров и магистров
+
+                    if (int.Parse(s[5]) < 5) bakalavr++; else magistr++; //Ошибка методички, в файле под этим индексом стоит возраст 
+                   
+                    
+                    //Задание 3.А
+                    if (int.Parse(s[6]) == 6) studentOnSixthCourse++;
+                    if (int.Parse(s[6]) == 5) studentOnFifthCourse++;
+                    
+
+
+                 
+
+
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine(e.Message);
+                    Console.WriteLine("Ошибка!ESC - прекратить выполнение программы");
+
+                    // Выход из Main
+                    if (Console.ReadKey().Key == ConsoleKey.Escape) return;
+                }
+            }
+            sr.Close();
+            list.Sort(new Comparison<Student>(MyDelegat));
+
+            Console.WriteLine("Всего студентов:" + list.Count);
+            Console.WriteLine("Магистров:{0}", magistr);
+            Console.WriteLine("Бакалавров:{0}", bakalavr);
+           
+            
+            
+            Console.WriteLine($"Студентов на 5 курсе: {studentOnFifthCourse} и на шестом: {studentOnSixthCourse}");
+            List<Student> studentsInfo = new List<Student>();
+            
+            
+
+          //Задание 2.Б
+                foreach (Student student in list) 
+                {
+                    if (student.age>=18 && student.age <=20) studentsInfo.Add(student);
+
+                    
+                }
+               
+                Console.WriteLine($"Количество студентов в возрасте от 18 до 20 лет : {studentsInfo.Count}");
+            foreach (Student student in studentsInfo) Console.WriteLine($"Возраст студента {student.age} лет, курс на котором учится студент {student.course} курс");
+ 
+
+            foreach (var v in list) Console.WriteLine(v.firstName);
+            Console.WriteLine(DateTime.Now - dt);
+            Console.ReadKey();
+
+        }
+    }
+}
+
+
+
+  
+
--- Hw6cs/Program.cs
+++ Hw6cs/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
@@ -7,21 +8,29 @@ using System.Threading.Tasks;
 
 namespace Hw6cs
 {
-    
+    delegate double Func(double x);
     internal class Program
     {
-        public static double F(double x)
+        public static double C(double x)
         {
             return x * x - 50 * x + 10;
         }
-        public static void SaveFunc(string fileName, double a, double b, double h)
+        public static double A(double x)
+        {
+            return x * 45 + 12 / x + 75;
+        }
+        public static double B(double x)
+        {
+            return x * x + 488 * x + 3;
+        }
+        public static void SaveFunc(Func func ,string fileName, double a, double b, double h)
         {
             FileStream fs = new FileStream(fileName, FileMode.Create, FileAccess.Write);
             BinaryWriter bw = new BinaryWriter(fs);
             double x = a;
             while (x <= b)
             {
-                bw.Write(F(x));
+                bw.Write(func(x));
                 x += h;// x=x+h;
             }
             bw.Close();
@@ -45,8 +54,49 @@ namespace Hw6cs
         }
         static void Main(string[] args)
         {
-            SaveFunc("data.bin", -100, 100, 0.5);
-            Console.WriteLine(Load("data.bin"));
+            Func func = A;
+            Func funcA = B;
+            Func funcB = C;
+
+           Func[] funcs = { func, funcA, funcB, };
+
+            
+            int choiseFunc = 0;
+            int choiseNumberOne = 0;
+            int choiseNumberTwo = 0;
+            //Задание 2
+            do { Console.WriteLine("Выберите функцию результат которой хотите увидеть:");
+                Console.WriteLine("1. Функция A");
+                Console.WriteLine("2. Функция B");
+                Console.WriteLine("3. Функция C");
+                Console.WriteLine("Или введите 0 чтобы завершить работу ");
+                
+                choiseFunc = int.Parse(Console.ReadLine());
+                Console.WriteLine("И введите два числа");
+                Console.WriteLine("Число номер 1:");
+                choiseNumberOne = int.Parse(Console.ReadLine());
+                Console.WriteLine("Число номер 2:");
+                choiseNumberTwo = int.Parse(Console.ReadLine());
+
+
+
+                switch (choiseFunc)
+                {
+                    case 1: SaveFunc(funcs[0], "data.bin", choiseNumberOne, choiseNumberTwo, 0.5);
+                        Console.WriteLine(Load("data.bin"));
+                        break;
+                    case 2: SaveFunc(funcs[1], "data.bin", choiseNumberOne, choiseNumberTwo, 0.5);
+                        Console.WriteLine(Load("data.bin"));
+                        break ;
+                    case 3: SaveFunc(funcs[2], "data.bin", choiseNumberOne, choiseNumberTwo, 0.5);
+                        Console.WriteLine(Load("data.bin"));
+                        break;
+                }
+                    
+                    
+                    } while (choiseFunc!=0);
+            
+            
             Console.ReadKey();
         }
     }
